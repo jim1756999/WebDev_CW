ac261b9a8ade8f221365e08b0cb2b0a00a02f634
jim1756999 <490854213@qq.com>
2021-12-25 18:39:49 +0800

    update

--- A2/js/scripts.js
+++ A2/js/scripts.js
@@ -1,13 +1,25 @@
-var introduction = document.getElementById("Introduction"); //获取父级标签的ID
+// Hold selected when mouse on submenu
+var introduction = document.getElementById("Introduction");
 function keep_hover() {
-    introduction.style.backgroundColor= "#8B8B8B";
-    introduction.style.color= "#E8E7E3";
-    introduction.style.boxShadow= "0 0 8px #8B8B8B"
-    introduction.style.transitionDuration= "0.4s"
+    introduction.style.backgroundColor = "#8B8B8B";
+    introduction.style.color = "#E8E7E3";
+    introduction.style.boxShadow = "0 0 8px #8B8B8B"
+    introduction.style.transitionDuration = "0.4s"
 }
 function keep_hover_leave() {
-    introduction.style.backgroundColor= "#E8E7E3";
-    introduction.style.color= "#4E4D4C";
-    introduction.style.boxShadow= ""
-    introduction.style.transitionDuration= "0.4s"
+    introduction.style.backgroundColor = "#E8E7E3";
+    introduction.style.color = "#4E4D4C";
+    introduction.style.boxShadow = ""
+    introduction.style.transitionDuration = "0.4s"
+}
+
+// TODO unable to get value
+
+function check() {
+    var v = document.forms["application"]["address"].value;
+    // if (v.length == "1100");
+        console.log(v);
+        document.write(v);
+        console.log("a");
+        return true
 }
