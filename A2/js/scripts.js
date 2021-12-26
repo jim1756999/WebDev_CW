2ffaa3d3c7968261fd4cbef8da520939319af7f2
jim1756999 <490854213@qq.com>
2021-12-26 14:19:26 +0800

    push without video

--- A2/js/scripts.js
+++ A2/js/scripts.js
@@ -13,9 +13,77 @@ function keep_hover_leave() {
     introduction.style.transitionDuration = "0.4s"
 }
 
-// TODO unable to get value
 
-function submit() {
-    var v = document.forms["application"]["address"].value;
-    
+function checkInfo() {
+    var firstname = document.forms["application"]["firstname"].value;
+    var phone = document.forms["application"]["phone"].value;
+    var sex = document.forms["application"]["sex"].value;
+    var date = document.forms["application"]["date"].value;
+    var lastname = document.forms["application"]["lastname"].value;
+    var email = document.forms["application"]["email"].value;
+    var address = document.forms["application"]["address"].value;
+    var age = document.forms["application"]["age"].value;
+    var event = document.forms["application"]["event"].value;
+    alert(
+        "Firstname: " + firstname + "\n" +
+        "Lastname: " + lastname + "\n" +
+        "Sex: " + sex + "\n" +
+        "Age: " + age + "\n" +
+        "Phone: " + phone + "\n" +
+        "Email: " + email + "\n" +
+        "Address: " + address + "\n" +
+        "Date: " + date + "\n"
+    )
+}
+
+function startTime() {
+    var today = new Date();
+    var { m, d, min, s, y, h } = getTime();
+    CHKZERO();
+    var day = Day();
+    document.getElementById('Date').innerHTML = y + "-" + m + "-" + d + " " + h + ":" + min + ":" + s + "   " + day;
+    setTimeout('startTime()', 1000);
+
+    function CHKZERO() {
+        m = chkzero(m);
+        d = chkzero(d);
+        min = chkzero(min);
+        s = chkzero(s);
+    }
+
+    function getTime() {
+        var y = today.getFullYear();
+        var m = today.getMonth() + 1;
+        var d = today.getDate();
+        var h = today.getHours();
+        var min = today.getMinutes();
+        var s = today.getSeconds();
+        return { m, d, min, s, y, h };
+    }
+
+    function Day() {
+        var day;
+        if (today.getDay() == 0)
+            day = "Sunday ";
+        if (today.getDay() == 1)
+            day = "Monday ";
+        if (today.getDay() == 2)
+            day = "Tuesday ";
+        if (today.getDay() == 3)
+            day = "Wednesday ";
+        if (today.getDay() == 4)
+            day = "Thursday ";
+        if (today.getDay() == 5)
+            day = "Friday ";
+        if (today.getDay() == 6)
+            day = "Saturday ";
+        return day;
+    }
+
+    function chkzero(i) {
+        if (i < 10) {
+            i = "0" + i;
+        }
+        return i;
+    }
 }
